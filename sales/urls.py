a17b21adf5fb1c51ad8527bb38e1d7e83a5e5db9
Mostafa-web656 <mostafasaeed9990@gmail.com>
2026-05-02 16:48:50 +0300

    fix invoices + auth

--- sales/urls.py
+++ sales/urls.py
@@ -1,3 +1,4 @@
+from . import views   # ✅ مهم جدًا
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -10,7 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/', api_root),  # 🔥 FIXED (بدل lambda الغبي)
-
+path("invoices/", views.invoices),
+    path("invoices/<int:id>/", views.invoice_detail),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
